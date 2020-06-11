ca6612ed08912e13204579bd1f6467b817dd2479
Maksim Koshman <maksimkoshman090@gmail.com>
2020-06-11 15:02:46 +0300

    create main components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +8,7 @@ const routes = [
     path: '/',
     name: 'Home',
     meta: { layout: 'main' },
-    component: Home
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
