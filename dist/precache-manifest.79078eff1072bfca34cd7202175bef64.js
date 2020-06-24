c3923e3db3bf438f47000756dced51040e79f1dc
Maksim Koshman <maksimkoshman090@gmail.com>
2020-06-24 14:41:02 +0300

    deploy

---
+++ dist/precache-manifest.79078eff1072bfca34cd7202175bef64.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ddafbb728bfa42bb0a",
+    "url": "/vue-crm/dist/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "6b257a553d4c5d3ebeae",
+    "url": "/vue-crm/dist/css/chunk-52679956.1d1c8876.css"
+  },
+  {
+    "revision": "936484218e8403353507",
+    "url": "/vue-crm/dist/css/chunk-vendors.3e4956bb.css"
+  },
+  {
+    "revision": "48288dd683c4881622987e19bc34c316",
+    "url": "/vue-crm/dist/index.html"
+  },
+  {
+    "revision": "68ddafbb728bfa42bb0a",
+    "url": "/vue-crm/dist/js/app.ccde1619.js"
+  },
+  {
+    "revision": "b86f5eedfc908354742c",
+    "url": "/vue-crm/dist/js/chunk-10b86dd9.d8c34da1.js"
+  },
+  {
+    "revision": "2d0853acbfc2d3cf9332",
+    "url": "/vue-crm/dist/js/chunk-2d0d7699.c3173e62.js"
+  },
+  {
+    "revision": "dfc5eddefc29d54c403e",
+    "url": "/vue-crm/dist/js/chunk-2d21a3d2.c590636a.js"
+  },
+  {
+    "revision": "6b257a553d4c5d3ebeae",
+    "url": "/vue-crm/dist/js/chunk-52679956.7dc1f553.js"
+  },
+  {
+    "revision": "39a0434b65e7e9f188de",
+    "url": "/vue-crm/dist/js/chunk-53c8ec65.a3f3c3e0.js"
+  },
+  {
+    "revision": "1b4f98a4233abfa82dd9",
+    "url": "/vue-crm/dist/js/chunk-5f4ad635.6183346d.js"
+  },
+  {
+    "revision": "bfd0d14700dee1450f71",
+    "url": "/vue-crm/dist/js/chunk-72449522.d38a35c2.js"
+  },
+  {
+    "revision": "3a093de2ba33b2573ce9",
+    "url": "/vue-crm/dist/js/chunk-c420df12.bae3cd5d.js"
+  },
+  {
+    "revision": "e77f7e4a0d04c4df463d",
+    "url": "/vue-crm/dist/js/chunk-cfa57870.beb0a667.js"
+  },
+  {
+    "revision": "936484218e8403353507",
+    "url": "/vue-crm/dist/js/chunk-vendors.e5bcbccb.js"
+  },
+  {
+    "revision": "6a8d2c9e18169fece64b94593566de0d",
+    "url": "/vue-crm/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-crm/dist/robots.txt"
+  }
+]);
